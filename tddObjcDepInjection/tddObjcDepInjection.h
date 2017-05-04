d90cc51df77378fc40d71395f9274deabbedd2d3
Steven Wu <swu@connectedlab.com>
2017-05-04 16:29:08 -0400

    initial project setup

---
+++ tddObjcDepInjection/tddObjcDepInjection.h
@@ -0,0 +1,19 @@
+//
+//  tddObjcDepInjection.h
+//  tddObjcDepInjection
+//
+//  Created by cl-dev on 2017-05-04.
+//  Copyright © 2017 Steven Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tddObjcDepInjection.
+FOUNDATION_EXPORT double tddObjcDepInjectionVersionNumber;
+
+//! Project version string for tddObjcDepInjection.
+FOUNDATION_EXPORT const unsigned char tddObjcDepInjectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tddObjcDepInjection/PublicHeader.h>
+
+
